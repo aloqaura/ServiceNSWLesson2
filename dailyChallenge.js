82faf560f8da7a531bcf2c21502adc10703fd9b9
Alex Burgess <alex.burgess3@studytafensw.edu.au>
2020-05-12 18:54:54 +1000

    dailyChallenges created

---
+++ dailyChallenge.js
@@ -0,0 +1,5 @@
+ // Write a console app that takes strings from the user and adds them to an array until the user 
+ // enters a duplicate entry. Then stop accepting user input and print each element in the array to
+ // the console. 
+
+ 
